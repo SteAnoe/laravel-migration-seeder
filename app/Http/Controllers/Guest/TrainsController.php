5fe55d11672c42fe641089e2abd636d1c6240f68
SteAnoe <ste.anoe@gmail.com>
2023-06-12 15:24:19 +0200

    finito

---
+++ app/Http/Controllers/Guest/TrainsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+class TrainsController extends Controller
+{
+    public function getTrain(){
+        $allTrains = Train::All();
+	    return view('welcome', compact('allTrains'));
+    }
+}
